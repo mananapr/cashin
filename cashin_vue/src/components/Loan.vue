76b3f719d90217fb3bcb1b9d19b03ed8751e1bed
Manan <manan.singh@digantara.co.in>
2024-11-09 22:31:39 +0530

    fix: replace <a> tags with <RouterLink> for navigation in multiple components

--- cashin_vue/src/components/Loan.vue
+++ cashin_vue/src/components/Loan.vue
@@ -55,8 +55,8 @@
         >
           Rs. {{ loan.cashback }} Cashback
         </a>
-        <a
-          :href="`/product/${loan.id}`"
+        <RouterLink
+          :to="`/product/${loan.id}`"
           class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Learn More
@@ -75,7 +75,7 @@
               d="M1 5h12m0 0L9 1m4 4L9 9"
             />
           </svg>
-        </a>
+        </RouterLink>
       </div>
     </div>
   </a>
